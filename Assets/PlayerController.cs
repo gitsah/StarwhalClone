aac7e80c8b4023d625c8281bcb016ed596ccc746
unknown <ghotbijr@gmail.com>
2017-10-12 05:07:39 -0700

    Changed some stuff, but mostly just confirmed that the physics I want aren't going to be ubtainable without significant work, since Unity isn't metting the requirements I was looking for. Setting this project aside for now.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("memes");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Quaternion rot = transform.rotation;
+        Vector3 pos = transform.position;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow)) { 
+            rot.z += 0.002f;
+            pos.y += 0.09f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rot.z -= 0.002f;
+            pos.y -= 0.09f;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.x += 0.09f;
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.x -= 0.09f;
+
+        }
+
+        transform.rotation = rot;
+        transform.position = pos;
+	}
+}
